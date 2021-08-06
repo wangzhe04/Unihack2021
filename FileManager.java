68b47ce65f1a37412ee5648930a0c55bf3de6677
wangzhe04 <67325944+wangzhe04@users.noreply.github.com>
2021-08-07 02:05:25 +0800

    Add files via upload
    Tested with data.csv, can get attributes without bug
    
    * didn't test if the attributes are correct, but likely so.

---
+++ FileManager.java
@@ -0,0 +1,67 @@
+///////////////////////File Header///////////////////////////
+//Author: Zhe Wang
+//Email: zwang2428@wisc.edu
+//Term: Summer2020
+////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * This class read in and add file into the dataStorage
+ * @author Zhe Wang
+ *
+ */
+public class FileManager {
+	DataStorage a;
+	public FileManager(File file, DataStorage a) throws IOException{
+		String line = "";
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		int end = 0;
+		int a2 = 0;
+		while((end != 2)){
+			try {
+				if((br.readLine() == null)) {
+					break;
+				}
+				if((line = br.readLine()).equals("")) {
+					end+=1;
+					continue;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println(9999);
+			}
+			end = 0;
+			
+			String[] array = line.split(",");
+			
+			String year = array[0].substring(0, 3);
+			//System.out.println(a2);
+			a2+=1;
+			String id = array[0].substring(4, 5);
+			String specific_time = array[0].substring(6, array[0].length()-1);
+			String lat = array[1];
+			String Long = array[2];
+			String Prs = array[3];
+			String Wnd = array[4];
+			
+			Typhoon t = new Typhoon(year, specific_time, lat, Long, Prs, Wnd);
+			a.insert(id, t);
+			
+
+		}
+		System.out.println(10086);
+	}
+
+
+}
